cdcf0c2d595e9a57670fb45f4eeab22868b7587e
Aayush Bharadwaj <bhardwajaayush0202@gmail.com>
2023-04-20 12:20:18 +0530

    latest

--- components/loader.jsx
+++ components/loader.jsx
@@ -4,7 +4,7 @@ import { Box, CircularProgress} from "@mui/material";
 
  const  Loader = () => {
   return (
-    <Box sx={{ display: 'flex',zIndex:"100",background: "transparent",height: "100vh",width: "100vw",top: "0",left: "0",right: "0",bottom: "0"}}>
+    <Box sx={{ display: 'flex',zIndex:"300",background: "  background: rgba(36, 30, 22, 0.2);",height: "100vh",width: "100vw",top: "0",left: "0",right: "0",bottom: "0",display:"flex",align: "center",justifyContent: "center"}}>
       <CircularProgress />
     </Box>
   )
